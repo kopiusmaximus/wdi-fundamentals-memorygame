5fb3ddde54d857f43d5c4f84e3d95ca3574f3db8
Alex Kopynec <alexander.kopynec@gmail.com>
2016-08-17 17:45:00 -0400

    deleted unused directories

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 
-// initialize variables
+// initalize the board variable and the necessary arrays
 var board = document.getElementById('game-board');
-var numCards = 4;
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-// create the board by creating & appending cards with the necessary attributes
+/* create the game board */
 var createBoard = function() {
+	// create the cards
 	for(var i = 0; i < cards.length; i++) {
 		var newCard = document.createElement('div');
+
+		// add the necessary attributes to each new card
 		newCard.className = 'card';
-		newCard.addEventListener('click', flipCard);
-		newCard.addEventListener('click', isTwoCards);
 		newCard.setAttribute('data-card', cards[i]);
 		newCard.innerHTML = "<img src='card_back.png' alt='back of card'>";
+
+		// add listeners to trigger functions when a card is clicked
+		newCard.addEventListener('click', flipCard);
+		newCard.addEventListener('click', isTwoCards);
+
+		// add each new card to the board
 		board.appendChild(newCard);
 	}
 }
 
-// determine whether two cards are a match
+/* determine whether two cards are a match */
 var isMatch = function(arr) {
 	var isMatch = (arr[0] === arr[1]);
 	return isMatch;
 }
 
-// flip over a card to show its face
+/* flip over a card to show its face */
 function flipCard() {
 	if (this.getAttribute('data-card') === 'queen') {
 		this.innerHTML = "<img src='queen_of_hearts.png' alt='queen of hearts'>"
@@ -33,21 +39,21 @@ function flipCard() {
 	}
 }
 
-// checks to see if there are cards in play
+/* check to see if there are cards in play */
 function isTwoCards() {
   // add card to array of cards in play
-  // 'this' hasn't been covered in this prework, but
-  // for now, just know it gives you access to the card the user clicked on
   cardsInPlay.push(this.getAttribute('data-card'));
 
   // if you have two cards in play check for a match
   if (cardsInPlay.length === 2) {
 
     // pass the cardsInPlay as an argument to isMatch function
+    // based on what isMatch returns, display appropriate message
 	if (isMatch(cardsInPlay)) {
 	    alert('You found a match!');
 	} else {
 		alert('Sorry, try again.');
+		// if it wasn't a match, flip the cards back over
 		var flipBack = document.getElementsByClassName('card');
 		for (var i = 0; i < flipBack.length; i++) {
 			flipBack[i].innerHTML = "<img src='card_back.png' alt='back of card'>";
